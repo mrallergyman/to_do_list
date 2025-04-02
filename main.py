3a038005a78424aa4773b2846162e9610637f31b
Denis Vovchenko <kosyaden@gmail.com>
2025-04-02 12:04:25 +0400

    Добавлено поле ввода, кнопки Add и Del, список дел. Добавление и удаление задачи сверху списка

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
-import tempfile, base64, zlib
+import tempfile
+import base64
+import zlib
 
 
 # прозрачная иконка приложения
-ICON = zlib.decompress(base64.b64decode(\
+ICON = zlib.decompress(base64.b64decode(
     "eJxjYGAEQgEBBiDJwZDBysAgxsDAoAHEQCEGBQaIOAg4sDIgACMUj4JRMApGwQgF\
     /ykEAFXxQRc="))
 
@@ -13,7 +15,22 @@ with open(ICON_PATH, "wb") as icon_file:
     icon_file.write(ICON)
 
 
-def add_to_json_file():  # добавляем задачи в JSON файл при закрытии окна
+# добавляем новую задачу
+def add_task():
+    task = entry.get()
+    if task:
+        listbox.insert(0, task)
+
+
+# удаляем задачу
+def del_task():
+    indexes = listbox.curselection()
+    print(indexes)
+    listbox.delete(indexes[0])
+
+
+# добавляем задачи в JSON файл при закрытии окна
+def add_to_json_file():
     root.destroy()  # ручное закрытие окна и всего приложения
     print("Закрытие приложения")
 
@@ -21,17 +38,37 @@ def add_to_json_file():  # добавляем задачи в JSON файл пр
 # Создаём главное окно
 root = tk.Tk()
 root.title("To-Do List")
-root.geometry("400x500")
+root.geometry("300x250")
 root.iconbitmap(default=ICON_PATH)
+root.columnconfigure(index=0, weight=4, )
+root.columnconfigure(index=1, weight=1)
+root.rowconfigure(index=0, weight=1)
+root.rowconfigure(index=1, weight=3)
+root.rowconfigure(index=2, weight=1)
 # root.attributes("-alpha", 0.5) # полупрозрачность
 
-label = tk.Label(text="Hello METANIT.COM")  # TODO создаем текстовую метку
-label.pack()    # размещаем метку в окне
+# TODO создаем текстовую метку
+# label = ttk.Label(text="To-Do List", font=("Tahoma", 12), padding=20)
+# label.pack()    # размещаем метку в окне
 
-btn_add = ttk.Button(text="Add Task", )  # создаем кнопку "Добавить задачу" из пакета ttk
-btn_add.pack()  # размещаем кнопку в окне
+# создаем поле ввода
+entry = ttk.Entry(font=("Tahoma", 10))
+entry.grid(column=0, row=0, padx=6, pady=6, sticky="ew")
 
-root.protocol("WM_DELETE_WINDOW", add_to_json_file)  # при закрытии окна вызывать функцию add_to_json_file
+# создаем кнопку "Добавить задачу" из пакета ttk
+btn_add = ttk.Button(text="Add Task", command=add_task)
+btn_add.grid(column=1, row=0, padx=6, pady=6)  # размещаем кнопку в окне
+
+# создаем список задач
+listbox = tk.Listbox()
+listbox.grid(row=1, column=0, columnspan=2, sticky="ew", padx=5, pady=5)
+
+# создаем кнопку "Удалить задачу" из пакета ttk
+btn_del = ttk.Button(text="Del Task", command=del_task)
+btn_del.grid(column=1, row=2, padx=6, pady=6)  # размещаем кнопку в окне
+
+# при закрытии окна вызывать функцию add_to_json_file
+root.protocol("WM_DELETE_WINDOW", add_to_json_file)
 
 # Запускаем приложение
 root.mainloop()
